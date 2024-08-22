a907bcb3f0d9868aa80757d35ab7c2c988d769c4
baodh <dang.hbaokody@gmail.com>
2024-08-22 13:24:30 +0700

    dsa-000: add config central service

---
+++ config_central/src/main/java/vn/baodh/config_central/ConfigCentralApplication.java
@@ -0,0 +1,13 @@
+package vn.baodh.config_central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCentralApplication.class, args);
+    }
+
+}
---
+++ config_central/src/test/java/vn/baodh/config_central/ConfigCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.baodh.config_central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
